4663c3b97b68390e1eb45a52fea52f568b241e39
notedit <notedit@gmail.com>
2018-03-04 16:18:31 +0800

    add pod

---
+++ dotEngine-demo/dotEngine-demo/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  dotEngine-demo
+//
+//  Created by xiang on 04/03/2018.
+//  Copyright © 2018 dotEngine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *joinButton;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)joinRoom:(id)sender {
+}
+
+@end
